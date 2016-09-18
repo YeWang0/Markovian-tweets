ffe5db3d5f49d81af2626ea9637ea4651ba5863e
YeWang0 <yewang0910@gmail.com>
2016-09-17 23:20:54 -0400

    sentence generator -nodejs

---
+++ sentence-generator.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var markov;
+var exefile='sentence-generator.py'
+var filename='portrait-of-the-artist.txt';
+const exec = require('child_process').exec;
+exec('python ./twitter/'+exefile+' ./twitter/result/'+filename, (error, stdout, stderr) => {
+if (error) {
+console.error(`exec error: ${error}`);
+return;
+}
+
+markov=`${stdout}`;
+console.log(markov);
+});
+
+app.get('/', function (req, res) {
+  res.send(markov);
+  
+	exec('python ./twitter/'+exefile+' ./twitter/result/'+filename, (error, stdout, stderr) => {
+    if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+    }
+  
+    markov=`${stdout}`;
+    console.log(markov);
+	});
+   
+    
+})
+
+
+var server = app.listen(8081, function () {
+  
+  console.log(`node start...`);
+  // console.log(`stderr: ${stderr}`);
+
+})

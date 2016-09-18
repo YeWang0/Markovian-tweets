7bfa608ab3f9b215dc979332a03131a0bf4bb263
YeWang0 <yewang0910@gmail.com>
2016-09-18 04:46:19 -0400

    set markovian chain length

--- sentence-generator.js
+++ sentence-generator.js
@@ -3,8 +3,9 @@ var app = express();
 var markov;
 var exefile='sentence-generator.py'
 var filename='portrait-of-the-artist.txt';
+var chain_len=4
 const exec = require('child_process').exec;
-exec('python ./twitter/'+exefile+' ./twitter/result/'+filename, (error, stdout, stderr) => {
+exec('python ./twitter/'+exefile+' ./twitter/result/'+filename+' '+chain_len, (error, stdout, stderr) => {
 if (error) {
 console.error(`exec error: ${error}`);
 return;
@@ -17,7 +18,7 @@ console.log(markov);
 app.get('/', function (req, res) {
   res.send(markov);
   
-	exec('python ./twitter/'+exefile+' ./twitter/result/'+filename, (error, stdout, stderr) => {
+	exec('python ./twitter/'+exefile+' ./twitter/result/'+filename+' '+chain_len, (error, stdout, stderr) => {
     if (error) {
     console.error(`exec error: ${error}`);
     return;
